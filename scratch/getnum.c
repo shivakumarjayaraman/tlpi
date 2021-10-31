37fc467a3c1501af209038633d5689c3efd6c26c
Shiva Jayaraman <shivajayaraman@gmail.com>
2021-10-31 20:52:20 +0530

    Added some more code

--- scratch/getnum.c
+++ scratch/getnum.c
@@ -1,6 +1,7 @@
 #include "tlpi_hdr.h"
 
 int main() {
-    const char *arg = "98";
+    const char *arg = "987";
     printf ("%d\n", getInt(arg, 0, "foo"));
+    printf ("%d\n", getInt(&arg[1], 0, "foo"));
 }
